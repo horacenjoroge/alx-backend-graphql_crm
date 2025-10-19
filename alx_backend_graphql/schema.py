57411d15ab7becc71ed8fccf983eed68025a82ae
“horacenjoroge” <125595726+horacenjorge@users.noreply.github.com>
2025-10-19 20:17:36 +0300

    fix :schema logic

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,10 +1,6 @@
-# alx_backend_graphql_crm/schema.py
-
 import graphene
-
 
 class Query(graphene.ObjectType):
     hello = graphene.String(default_value="Hello, GraphQL!")
-
 
 schema = graphene.Schema(query=Query)
